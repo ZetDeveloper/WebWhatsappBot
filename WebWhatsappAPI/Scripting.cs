5ca8b920567e4caa15e08e064380873ea0d9635d
zet.developer <zet.daniel@outlook.com>
2017-11-18 11:37:11 -0600

    Get groups list

--- WebWhatsappAPI/Scripting.cs
+++ WebWhatsappAPI/Scripting.cs
@@ -27,5 +27,58 @@ namespace WebWhatsappBotCore
                                                 }
                                                 return false;
                                            ";
+
+
+
+        //Script for send a message by id, example id: 52111111111@c.us
+        public static string GetGroups = @"
+
+                                                var GroupOutput = [];
+
+                                                for (group in Groups) {
+                                                    if (isNaN(group)) {
+                                                        continue;
+                                                    }
+
+                                                    //if(!(Groups[group].__x_id.toLowerCase().indexOf('@broadcast') >= 0))
+
+                                                    var group_name = ggn(Groups[group].__x_id);
+
+                                                    var i = Store.GroupMetadata.models[group].participants.models;
+                                                    var ii = [];
+
+                                                    for (p in i) {
+                                                        if (isNaN(group)) {
+                                                            continue;
+                                                        };
+                                                        var n = Store.GroupMetadata.models[group].participants.models[p].__x_id;
+                                                        var m = ggn(Store.GroupMetadata.models[group].participants.models[p].__x_id);
+                                                        if (n == null){
+                                                            continue;
+                                                        }
+                                                        ii.push(n);
+
+                                                    }
+                                                    GroupOutput.push({
+                                                        'Group' : {'Name' :group_name, 'Participants' : ii }
+                                                    });
+
+                                                }
+
+                                                function ggn(pno) {
+                                                    var contacts = window.Store.Contact.models;
+                                                        for(var i in contacts){
+                                                        if(isNaN(i)) {
+                                                            continue;
+                                                        }
+                                                        if(pno == contacts[i].__x_id){
+                                                            return (contacts[i].__x_name);
+                                                        }
+                                                    }
+                                                }
+                                                return GroupOutput;
+                                                console.log(GroupOutput);
+                                           ";
+
     }
 }
